6914746499800742319cf375a7f2a27f70d13741
yakumaru-yaku <yakumaru.engineer@gmail.com>
2020-05-13 01:09:15 +0900

    first

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+
+// カレンダー
+
+$d = isset($_GET['d']) ? $_GET['d'] : date("Y-m-d"); // 2011-11-25
+
+list($year, $month, $day) = explode("-", $d);
+
+if (!checkdate($month, $day, $year)) {
+    echo "不正な日付です";
+    exit;
+}
+
+$prev = date("Y-m-d", mktime(0,0,0, $month-1, 1, $year)); // 2011-10-01
+$next = date("Y-m-d", mktime(0,0,0, $month+1, 1, $year)); // 2011-12-01
+
+$fd = date("w", strtotime("$year-$month-1"));
+$ld = date("t", strtotime("$year-$month-1"));
+
+$s = '<tr>';
+
+$y =  0;
+
+while ($fd--) {
+    $y++;
+    $s .= '<td></td>';
+}
+
+for ($i=1; $i<=$ld; $i++) {
+    if($y % 7 == 0) {
+        $s .= "</tr><tr>";
+        $y = 0;
+    }
+    switch ($y) {
+        case 0:
+            $s .= "<td class=\"sun\">$i</td>";
+            break;
+        case 6:
+            $s .= "<td class=\"sat\">$i</td>";
+            break;
+        default:
+            $s .= "<td>$i</td>";
+            break;
+    }
+    $y++;
+}
+
+$r = 7 - $y;
+
+while ($r--) {
+    $s .= '<td></td>';
+}
+
+$s .= "</tr>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8" />
+        <title>PHPの練習</title>
+        <style>
+            td {
+                padding: 5px;
+                border: 1px solid #ccc;
+                text-align: right;
+            }
+            th {
+                padding: 5px;
+                border: 1px solid #ccc;
+                text-align: center;
+                background-color: #eee;
+            }
+            th.sun, td.sun {
+                color: red;
+                font-weight: bold;
+            }
+            th.sat, td.sat {
+                color: blue;
+                font-weight: bold;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>PHPの練習</h1>
+        <table>
+        <tr><th><a href="?d=<?php echo $prev; ?>">←</a></th><th colspan="5"><?php echo htmlspecialchars($year); ?>年<?php echo htmlspecialchars($month); ?>月</th><th><a href="?d=<?php echo $next; ?>">→</th></tr>
+        <tr><th class="sun">日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th class="sat">土</th></tr>
+        <?php echo $s; ?>
+        </table>
+    </body>
+</html>
